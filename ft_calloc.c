3a8e40a22cab041cf1577bc8e41349e893476136
Julia Batoro <juliabatoro@MacBook-Pro-de-Julia.local>
2021-12-06 11:06:59 +0100

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -15,7 +15,7 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	ptr = malloc(count * size);
+	ptr = malloc(count * size); //nb de blocs * taille d'un bloc
 	if (ptr == NULL)
 		return (ptr);
 	ft_bzero(ptr, size * count);
