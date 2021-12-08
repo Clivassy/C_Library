94b91fe1ca58894da839fbef2d0c31809bfaee97
Julia BATORO <jbatoro@e1r8p1.42.fr>
2021-12-08 14:53:22 +0100

    test listes

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,10 +6,33 @@
 /*   By: jbatoro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 15:49:27 by jbatoro           #+#    #+#             */
-/*   Updated: 2021/12/05 17:46:52 by jbatoro          ###   ########.fr       */
+/*   Updated: 2021/12/08 14:50:03 by jbatoro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
+/*
+typedef struct	s_list
+{
+	void	*content;
+	struct	s_list	*next;
+}			t_list
+*/
+int	ft_lstsize(t_list *lst)
+{
+	int		nb_element;
+	
+	nb_element = 0;
+	if (lst)
+	{
+		while (lst)
+		{
+			lst = lst->next;
+			nb_element++;
+		}
+	}
+	return (nb_element);
+}
 
 t_list	*ft_lstnew(void *content)
 {
@@ -21,4 +44,34 @@ t_list	*ft_lstnew(void *content)
 	element->content = content;
 	element->next = NULL;
 	return (element);
+}
+
+void	ft_putstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+}
+
+void	ft_print_list(t_list *lst)
+{
+	while (lst)
+	{
+		ft_putstr(lst->content);
+		lst = lst->next;
+	}
+}
+
+int main()
+{
+	t_list *lst;
+	char str[] = "Julia";
+	
+	lst = ft_lstnew(str);
+	ft_print_list(lst);
 }
